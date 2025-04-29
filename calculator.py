b265056f43aceacc3c6ab7e6289dedec966b8ec0
Rakshitdev Ror <113888745+rorrakshit@users.noreply.github.com>
2025-04-29 10:12:55 +0530

    Add files via upload
    Python program to perform addition, subtraction, multiplication and divide operations.

---
+++ calculator.py
@@ -0,0 +1,12 @@
+fn = float(input("Enter first number: \t"))
+sn = float(input("Enter second number: \t"))
+
+ad = fn + sn
+sub = abs(fn - sn)
+mul = fn *sn
+div = fn / sn
+
+print("Addition: \t",ad)
+print("Subtraction: \t",sub)
+print("Multiply: \t",mul)
+print("Divide: \t",div)
